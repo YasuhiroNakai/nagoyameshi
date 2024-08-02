6a022f3170c1dc5bc4d4c5ff339f296a611f2bc2
yasuhironakai <to_yasuhiro_nakai@yahoo.co.jp>
2024-08-02 11:39:34 +0900

    deploy to heroku

--- nagoyameshi/settings.py
+++ nagoyameshi/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'nagoyameshi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_PTH, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
